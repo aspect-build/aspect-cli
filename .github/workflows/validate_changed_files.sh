b74ed2d91145c11a25e2ab3b8d92d74e55c3ba92
Jesse Tatasciore <jessetatasciore7@gmail.com>
2022-01-07 09:43:05 -0500

    fix(verify): Fetch the latest commit from the branches in question

--- .github/workflows/validate_changed_files.sh
+++ .github/workflows/validate_changed_files.sh
@@ -5,7 +5,12 @@ set -o errexit -o nounset -o pipefail
 INVALID_FILE_PATHS=('pkg/plugin/sdk') # Array of filepaths that are not allowed
 VALID_FILE_PATHS=('pkg/plugin/sdk/v1alpha2') # Array of filepaths that are allowed
 
-git diff $1..$2 --name-only | while read -r file; do
+# by default the only local branch will be pull/PR#/merge
+# fetch only the latest commit from the 2 branches in question to avoid fetching the entire repo which could be costly
+git fetch --depth 1 origin "$1"
+git fetch --depth 1 origin "$2"
+
+git diff "origin/$1..origin/$2" --name-only | while read -r file; do
 
     # check if filepath matches a valid path. If so move to the next change
     for valid_path in "${VALID_FILE_PATHS[@]}"; do
