da3a1e15776e406c29b66b4defa05e1852af0721
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-01-07 16:44:47 -0800

    feat: run command (#103)
    * feat: run command
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
    
    * fix: TODO from help msg
    
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

---
+++ cmd/aspect/run/run.go
@@ -0,0 +1,64 @@
+/*
+Copyright © 2021 Aspect Build Systems
+
+Not licensed for re-use
+*/
+
+package run
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+
+	"aspect.build/cli/pkg/aspect/run"
+	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/interceptors"
+	"aspect.build/cli/pkg/ioutils"
+	"aspect.build/cli/pkg/plugin/system"
+	"aspect.build/cli/pkg/plugin/system/bep"
+)
+
+// NewDefaultRunCmd creates a new run cobra command with the default
+// dependencies.
+func NewDefaultRunCmd(pluginSystem system.PluginSystem) *cobra.Command {
+	return NewRunCmd(
+		ioutils.DefaultStreams,
+		pluginSystem,
+		bazel.New(),
+	)
+}
+
+func NewRunCmd(
+	streams ioutils.Streams,
+	pluginSystem system.PluginSystem,
+	bzl bazel.Bazel,
+) *cobra.Command {
+	return &cobra.Command{
+		Use:   "run",
+		Short: "Builds the specified target and runs it with the given arguments.",
+		// TODO(f0rmiga): the following comment from 'bazel --help run' may not
+		// be what we want to provide to our users.
+		Long: `'run' accepts any 'build' options, and will inherit any defaults
+provided by .bazelrc.
+
+If your script needs stdin or execution not constrained by the bazel lock,
+use 'bazel run --script_path' to write a script and then execute it.
+`,
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				interceptors.WorkspaceRootInterceptor(),
+				pluginSystem.BESBackendInterceptor(),
+			},
+			func(ctx context.Context, cmd *cobra.Command, args []string) (exitErr error) {
+				// TODO(f0rmiga): post-run hook.
+
+				workspaceRoot := ctx.Value(interceptors.WorkspaceRootKey).(string)
+				bzl.SetWorkspaceRoot(workspaceRoot)
+				r := run.New(streams, bzl)
+				besBackend := ctx.Value(system.BESBackendInterceptorKey).(bep.BESBackend)
+				return r.Run(args, besBackend)
+			},
+		),
+	}
+}
