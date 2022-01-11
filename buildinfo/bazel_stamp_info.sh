b46691ae56d86e493c6fe46d6c916bdda38786c5
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-01-10 23:47:09 -0800

    fix: No names found, cannot describe anything
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- buildinfo/bazel_stamp_info.sh
+++ buildinfo/bazel_stamp_info.sh
@@ -21,4 +21,7 @@ echo "BUILD_TIME $(date "+%Y-%m-%d %H:%M:%S %Z")"
 # Note, BUILD_USER is automatically available in the stable-status.txt, it matches $USER
 echo "STABLE_BUILD_SCM_SHA $(git rev-parse HEAD)"
 echo "STABLE_BUILD_SCM_LOCAL_CHANGES $(has_local_changes)"
-echo "STABLE_BUILD_SCM_TAG $(git describe --tags)"
+
+if [ "$(git tag | wc -l)" -gt 0 ]; then
+    echo "STABLE_BUILD_SCM_TAG $(git describe --tags)"
+fi
