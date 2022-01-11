4a7f484d19fc84df17a0896fa223da2aefe1c711
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-01-11 01:13:54 -0800

    feat: enforce generation of docs for commands
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- cmd/docgen/main.go
+++ cmd/docgen/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 
 	"aspect.build/cli/cmd/aspect/root"
@@ -12,9 +13,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatal("Usage: cmd/docgen /path/to/outdir")
-	}
+	cmd := &cobra.Command{Use: "docgen"}
 
 	pluginSystem := system.NewPluginSystem()
 	if err := pluginSystem.Configure(ioutils.DefaultStreams); err != nil {
@@ -22,8 +21,53 @@ func main() {
 	}
 	defer pluginSystem.TearDown()
 
-	err := doc.GenMarkdownTree(root.NewDefaultRootCmd(pluginSystem), os.Args[1])
-	if err != nil {
+	aspectRootCmd := root.NewDefaultRootCmd(pluginSystem)
+
+	cmd.AddCommand(NewCommandListCmd(aspectRootCmd))
+	cmd.AddCommand(NewGenMarkdownCmd(aspectRootCmd))
+
+	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func NewCommandListCmd(aspectRootCmd *cobra.Command) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "command-list",
+		Short: "Creates a .bzl file with the top-level list of commands of the aspect CLI.",
+		RunE: func(_ *cobra.Command, _ []string) (exitErr error) {
+			fmt.Println(`"""Generated file - do NOT edit!`)
+			fmt.Println("This module contains the list of top-level commands from the aspect CLI.")
+			fmt.Println(`"""`)
+			fmt.Println("COMMAND_LIST = [")
+			cmds := aspectRootCmd.Commands()
+			for _, cmd := range cmds {
+				if cmd.IsAvailableCommand() {
+					fmt.Printf("    %q,\n", cmd.Use)
+				}
+			}
+			fmt.Println("]")
+			return nil
+		},
+	}
+	return cmd
+}
+
+func NewGenMarkdownCmd(aspectRootCmd *cobra.Command) *cobra.Command {
+	var outputDir string
+
+	cmd := &cobra.Command{
+		Use:   "gen-markdown",
+		Short: "Generates the markdown documentation.",
+		RunE: func(_ *cobra.Command, _ []string) error {
+			if outputDir == "" {
+				return fmt.Errorf("missing --output-dir")
+			}
+			return doc.GenMarkdownTree(aspectRootCmd, outputDir)
+		},
+	}
+
+	cmd.PersistentFlags().StringVar(&outputDir, "output-dir", "", "The path to the output directory.")
+
+	return cmd
 }
---
+++ docs/command_list.bzl
@@ -0,0 +1,12 @@
+"""Generated file - do NOT edit!
+This module contains the list of top-level commands from the aspect CLI.
+"""
+COMMAND_LIST = [
+    "build",
+    "clean",
+    "docs",
+    "info",
+    "run",
+    "test",
+    "version",
+]
