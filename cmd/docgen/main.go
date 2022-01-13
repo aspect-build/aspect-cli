7f690cea77b6d6ade94671ad4927803905597463
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-01-13 09:31:39 -0800

    refactor: docgen subcommand rename and description
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- cmd/docgen/main.go
+++ cmd/docgen/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	aspectRootCmd := root.NewDefaultRootCmd(pluginSystem)
 
-	cmd.AddCommand(NewCommandListCmd(aspectRootCmd))
+	cmd.AddCommand(NewBzlCommandListCmd(aspectRootCmd))
 	cmd.AddCommand(NewGenMarkdownCmd(aspectRootCmd))
 
 	if err := cmd.Execute(); err != nil {
@@ -31,10 +31,12 @@ func main() {
 	}
 }
 
-func NewCommandListCmd(aspectRootCmd *cobra.Command) *cobra.Command {
+func NewBzlCommandListCmd(aspectRootCmd *cobra.Command) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "command-list",
-		Short: "Creates a .bzl file with the top-level list of commands of the aspect CLI.",
+		Use:   "bzl-command-list",
+		Short: "Prints a .bzl file with the top-level list of commands of the aspect CLI.",
+		Long: "This command is used to produce the .bzl file with the list of top-level commands" +
+			"used to automatically generate markdown documentation for this repository.",
 		RunE: func(_ *cobra.Command, _ []string) (exitErr error) {
 			fmt.Println(`"""Generated file - do NOT edit!`)
 			fmt.Println("This module contains the list of top-level commands from the aspect CLI.")
