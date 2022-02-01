028fa393988c9f3475c35de40b3bcd754400c190
Jesse Tatasciore <jessetatasciore7@gmail.com>
2022-02-01 15:30:07 -0500

    fix: Set bazel workspace root for the bazel query verbs

--- cmd/aspect/cquery/cquery.go
+++ cmd/aspect/cquery/cquery.go
@@ -7,10 +7,13 @@ Not licensed for re-use
 package cquery
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 
 	"aspect.build/cli/pkg/aspect/cquery"
 	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/interceptors"
 	"aspect.build/cli/pkg/ioutils"
 )
 
@@ -19,13 +22,21 @@ func NewDefaultCQueryCmd() *cobra.Command {
 }
 
 func NewCQueryCommand(streams ioutils.Streams, bzl bazel.Bazel) *cobra.Command {
-	q := cquery.New(streams, bzl, true)
-
 	cmd := &cobra.Command{
 		Use:   "cquery",
 		Short: "Executes a cquery.",
 		Long:  "Executes a query language expression over a specified subgraph of the build dependency graph using cquery.",
-		RunE:  q.Run,
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				interceptors.WorkspaceRootInterceptor(),
+			},
+			func(ctx context.Context, cmd *cobra.Command, args []string) (exitErr error) {
+				workspaceRoot := ctx.Value(interceptors.WorkspaceRootKey).(string)
+				bzl.SetWorkspaceRoot(workspaceRoot)
+				q := cquery.New(streams, bzl, true)
+				return q.Run(cmd, args)
+			},
+		),
 	}
 
 	return cmd
