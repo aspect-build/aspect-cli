46261144755f3f36bb35e347e035cb238b052ed0
Jesse Tatasciore <jessetatasciore7@gmail.com>
2022-02-01 15:46:48 -0500

    fix: Set bazel workspace root for the bazel query verbs (#128)

--- cmd/aspect/aquery/aquery.go
+++ cmd/aspect/aquery/aquery.go
@@ -7,10 +7,13 @@ Not licensed for re-use
 package aquery
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 
 	"aspect.build/cli/pkg/aspect/aquery"
 	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/interceptors"
 	"aspect.build/cli/pkg/ioutils"
 )
 
@@ -19,13 +22,21 @@ func NewDefaultAQueryCmd() *cobra.Command {
 }
 
 func NewAQueryCommand(streams ioutils.Streams, bzl bazel.Bazel) *cobra.Command {
-	q := aquery.New(streams, bzl, true)
-
 	cmd := &cobra.Command{
 		Use:   "aquery",
 		Short: "Executes an aquery.",
 		Long:  "Executes a query language expression over a specified subgraph of the build dependency graph using aquery.",
-		RunE:  q.Run,
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				interceptors.WorkspaceRootInterceptor(),
+			},
+			func(ctx context.Context, cmd *cobra.Command, args []string) (exitErr error) {
+				workspaceRoot := ctx.Value(interceptors.WorkspaceRootKey).(string)
+				bzl.SetWorkspaceRoot(workspaceRoot)
+				q := aquery.New(streams, bzl, true)
+				return q.Run(cmd, args)
+			},
+		),
 	}
 
 	return cmd
--- cmd/aspect/query/query.go
+++ cmd/aspect/query/query.go
@@ -7,10 +7,13 @@ Not licensed for re-use
 package query
 
 import (
+	"context"
+
 	"github.com/spf13/cobra"
 
 	"aspect.build/cli/pkg/aspect/query"
 	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/interceptors"
 	"aspect.build/cli/pkg/ioutils"
 )
 
@@ -19,13 +22,21 @@ func NewDefaultQueryCmd() *cobra.Command {
 }
 
 func NewQueryCommand(streams ioutils.Streams, bzl bazel.Bazel) *cobra.Command {
-	q := query.New(streams, bzl, true)
-
 	cmd := &cobra.Command{
 		Use:   "query",
 		Short: "Executes a dependency graph query.",
 		Long:  "Executes a query language expression over a specified subgraph of the build dependency graph.",
-		RunE:  q.Run,
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				interceptors.WorkspaceRootInterceptor(),
+			},
+			func(ctx context.Context, cmd *cobra.Command, args []string) (exitErr error) {
+				workspaceRoot := ctx.Value(interceptors.WorkspaceRootKey).(string)
+				bzl.SetWorkspaceRoot(workspaceRoot)
+				q := query.New(streams, bzl, true)
+				return q.Run(cmd, args)
+			},
+		),
 	}
 
 	return cmd
