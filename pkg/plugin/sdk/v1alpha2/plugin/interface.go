abe8a86e0eb9b313253bcc1d7cfb7f27f9672f62
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-02-08 16:29:49 -0800

    feat: plugin.Base
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- pkg/plugin/sdk/v1alpha2/plugin/interface.go
+++ pkg/plugin/sdk/v1alpha2/plugin/interface.go
@@ -27,3 +27,30 @@ type Plugin interface {
 		promptRunner ioutils.PromptRunner,
 	) error
 }
+
+// Base satisfies the Plugin interface. For plugins that only implement a subset
+// of the Plugin interface, using this as a base will give the advantage of not
+// needing to implement the empty methods.
+type Base struct{}
+
+var _ Plugin = (*Base)(nil)
+
+// BEPEventCallback satisfies Plugin.BEPEventCallback.
+func (*Base) BEPEventCallback(*buildeventstream.BuildEvent) error {
+	return nil
+}
+
+// PostBuildHook satisfies Plugin.PostBuildHook.
+func (*Base) PostBuildHook(bool, ioutils.PromptRunner) error {
+	return nil
+}
+
+// PostTestHook satisfies Plugin.PostTestHook.
+func (*Base) PostTestHook(bool, ioutils.PromptRunner) error {
+	return nil
+}
+
+// PostRunHook satisfies Plugin.PostRunHook.
+func (*Base) PostRunHook(bool, ioutils.PromptRunner) error {
+	return nil
+}
