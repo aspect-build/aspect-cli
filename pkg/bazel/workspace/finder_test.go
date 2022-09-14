14f49b9afdd09e3528bf484897e31d64e4e15a63
Alex Eagle <alex@aspect.dev>
2022-09-14 14:32:21 -0700

    chore: update license header in code to match LICENSE (#249)

--- pkg/bazel/workspace/finder_test.go
+++ pkg/bazel/workspace/finder_test.go
@@ -1,9 +1,17 @@
 /*
  * Copyright 2022 Aspect Build Systems, Inc.
  *
- * Licensed under the aspect.build Commercial License (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * Full License text is in the LICENSE file included in the root of this repository.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package workspace
