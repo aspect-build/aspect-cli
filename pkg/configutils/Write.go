dd1fe65198312c825307f00deaecc64428cf4100
Chuck Grindel <chuck@chuckgrindel.com>
2022-09-14 18:04:15 -0600

    Add placeholder for tests

--- pkg/configutils/Write.go
+++ pkg/configutils/Write.go
@@ -1,38 +1,17 @@
 package configutils
 
 import (
-	"log"
-
 	"github.com/spf13/viper"
 )
 
 func Write(v *viper.Viper) error {
 	// Workaround for issue with WriteConfig
 	// https://github.com/spf13/viper/issues/433
-
-	// DEBUG BEGIN
-	log.Printf("*** CHUCK: START")
-	// DEBUG END
-
-	if err := v.WriteConfig(); err != nil {
-		// DEBUG BEGIN
-		log.Printf("*** CHUCK:  err: %+#v", err)
-		// DEBUG END
-		// var fileNotFoundError *viper.ConfigFileNotFoundError
-		// if errors.As(err, &fileNotFoundError) {
-		// if errors.Is(err, viper.ConfigFileNotFoundError) {
-		// if _, ok := err.(*viper.ConfigFileNotFoundError); ok {
+	var err error
+	if err = v.WriteConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			// DEBUG BEGIN
-			log.Printf("*** CHUCK: NotFoundErr")
-			// DEBUG END
-			if err = v.SafeWriteConfig(); err != nil {
-				// DEBUG BEGIN
-				log.Printf("*** CHUCK:  err: %v", err)
-				// DEBUG END
-				return err
-			}
+			err = v.SafeWriteConfig()
 		}
 	}
-	return nil
+	return err
 }
---
+++ pkg/configutils/Write_test.go
@@ -0,0 +1,12 @@
+package configutils_test
+
+import "testing"
+
+func TestWrite(t *testing.T) {
+	t.Run("config file does not exist", func(t *testing.T) {
+		t.Error("IMPLEMENT ME!")
+	})
+	t.Run("config file does exist", func(t *testing.T) {
+		t.Error("IMPLEMENT ME!")
+	})
+}
