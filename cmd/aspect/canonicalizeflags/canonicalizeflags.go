6134aa002cc7e867e3ffc505aebca6c3d29af5a4
Greg Magolan <greg@aspect.dev>
2022-09-19 11:03:29 -0700

    feat: add canonicalize-flags pass-through command (#263)

---
+++ cmd/aspect/canonicalizeflags/canonicalizeflags.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package canonicalizeflags
+
+import (
+	"github.com/spf13/cobra"
+
+	"aspect.build/cli/pkg/aspect/canonicalizeflags"
+	"aspect.build/cli/pkg/aspect/root/flags"
+	"aspect.build/cli/pkg/interceptors"
+	"aspect.build/cli/pkg/ioutils"
+)
+
+func NewDefaultCanonicalizeFlagsCmd() *cobra.Command {
+	return NewCanonicalizeFlagsCmd(ioutils.DefaultStreams)
+}
+
+func NewCanonicalizeFlagsCmd(streams ioutils.Streams) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "canonicalize-flags",
+		Short: "Canonicalizes a list of bazel options.",
+		Long: `This command canonicalizes a list of bazel options.
+
+Don't forget to prepend  '--' to end option parsing before the flags to canonicalize.`,
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				flags.FlagsInterceptor(streams),
+			},
+			canonicalizeflags.New(streams).Run,
+		),
+	}
+
+	return cmd
+}
