cbc7af3f5e9040c4e6b086ffe117fd50592f9e74
Chuck Grindel <chuck@chuckgrindel.com>
2022-09-19 18:47:26 -0600

    Add integration test for init

---
+++ integration_tests/bootstrap/init_test.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# --- begin runfiles.bash initialization v2 ---
+# Copy-pasted from the Bazel Bash runfiles library v2.
+set -uo pipefail; f=bazel_tools/tools/bash/runfiles/runfiles.bash
+# shellcheck disable=SC1090
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$0.runfiles/$f" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+  { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=; set -e
+# --- end runfiles.bash initialization v2 ---
+
+# Locate Deps
+
+assertions_sh_location=aspect_bazel_lib/shlib/lib/assertions.sh
+assertions_sh="$(rlocation "${assertions_sh_location}")" || \
+  (echo >&2 "Failed to locate ${assertions_sh_location}" && exit 1)
+# shellcheck disable=SC1090
+source "${assertions_sh}"
+
+# Variables
+
+bootstrap_location=build_aspect_cli/cmd/bootstrap
+bootstrap="$(rlocation "${bootstrap_location}")/bootstrap_/bootstrap" || \
+  (echo >&2 "Failed to locate ${bootstrap_location}" && exit 1)
+
+# MARK - Test
+
+# Set up workspace
+echo 'workspace(name = "temp_workspace")' > WORKSPACE
+
+$bootstrap init
+bazel_version_path="${PWD}/.bazelversion"
+[[ -f "${bazel_version_path}" ]] || fail ".bazelversion does not exist"
+bazel_version_contents="$(< "${bazel_version_path}")"
+assert_match "aspect-build/no release" "${bazel_version_contents}" "missing aspect CLI version"
