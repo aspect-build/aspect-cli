0b323094cf213873cd9d6b44daafbad8c904e120
Alex Eagle <alex@aspect.dev>
2022-10-17 15:41:50 -0700

    feat: add 'print' command (#507)
    Fixes https://github.com/aspect-build/aspect-cli/issues/197
    
    GitOrigin-RevId: bac8eaba6df1ece4e7473ec6f5061a3a5733cbcf

--- cmd/aspect/root/root.go
+++ cmd/aspect/root/root.go
@@ -35,6 +35,7 @@ import (
 	"aspect.build/cli/cmd/aspect/info"
 	"aspect.build/cli/cmd/aspect/mobileinstall"
 	"aspect.build/cli/cmd/aspect/modquery"
+	"aspect.build/cli/cmd/aspect/print"
 	"aspect.build/cli/cmd/aspect/printaction"
 	"aspect.build/cli/cmd/aspect/query"
 	"aspect.build/cli/cmd/aspect/run"
@@ -75,6 +76,7 @@ func NewRootCmd(
 
 	flags.AddGlobalFlags(cmd, defaultInteractive)
 	cmd.AddGroup(&cobra.Group{ID: "common", Title: "Common Bazel Commands:"})
+	cmd.AddGroup(&cobra.Group{ID: "aspect", Title: "Commands only in Aspect CLI:"})
 	cmd.AddGroup(&cobra.Group{ID: "plugin", Title: "Custom Commands from Plugins:"})
 	cmd.AddGroup(&cobra.Group{ID: "built-in", Title: "Other Bazel Built-in Commands:"})
 
@@ -93,6 +95,7 @@ func NewRootCmd(
 	// license
 	cmd.AddCommand(mobileinstall.NewDefaultMobileInstallCmd())
 	cmd.AddCommand(modquery.NewDefaultModQueryCmd())
+	cmd.AddCommand(print.NewDefaultPrintCmd())
 	cmd.AddCommand(printaction.NewDefaultPrintActionCmd())
 	cmd.AddCommand(query.NewDefaultQueryCmd())
 	cmd.AddCommand(run.NewDefaultRunCmd(pluginSystem))
