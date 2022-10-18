ee365cab07879287af33e8aafa145a8e006377ce
Alex Eagle <alex@aspect.dev>
2022-10-17 21:17:45 -0700

    docs(cli): improve docstring for run (#517)
    GitOrigin-RevId: 480f1911fb5ddb5bccfe8097c5c2c3a95d604343

--- cmd/aspect/run/run.go
+++ cmd/aspect/run/run.go
@@ -46,17 +46,37 @@ func NewRunCmd(
 	bzlProvider bazel.BazelProvider,
 ) *cobra.Command {
 	return &cobra.Command{
-		Use:   "run",
+		Use:   "run [--run_under=command-prefix] <target> -- [args for program ...]",
+		Args:  cobra.ExactArgs(1),
 		Short: "Build a single target and run it with the given arguments",
-		// TODO(f0rmiga): the following comment from 'bazel --help run' may not
-		// be what we want to provide to our users.
-		Long: `'run' accepts any 'build' options, and will inherit any defaults
-provided by .bazelrc.
+		Long: `Equivalent to 'aspect build <target>' followed by spawning the resulting executable.
+
+Documentation: <https://bazel.build/docs/user-manual#running-executables>
+
+Two environment variables will be present that the program may reference:
+- ` + "`BUILD_WORKSPACE_DIRECTORY`" + `: the root of the workspace where the build was run.
+- ` + "`BUILD_WORKING_DIRECTORY`" + `: the current working directory where Bazel was run from.
+
+Note that the <target> may have an 'args' and 'env' attribute. The 'run' command honors these and
+sets the arguments and environment of the spawned executable, unlike if the binary is executed as
+an action during a build step, or is run directly outside of Bazel.
+See <https://bazel.build/reference/be/common-definitions#common-attributes-binaries>.
+
+'run' accepts any 'build' options, and will inherit any defaults provided by .bazelrc.
 
 If your script needs stdin or execution not constrained by the bazel lock,
 use 'bazel run --script_path' to write a script and then execute it.
+
+By default, the program is run with a working directory inside $(aspect info execution_root).
+Some programs expect to be run under a certain working directory, such as the workspace root.
+Use the [--run_under](https://bazel.build/docs/user-manual#run_under) flag with a cd command, like
+` + "`aspect run --run_under=\"cd $PWD &&\" //my:program`" + ` to use the current directory.
+Another common approach if the program's code is in your repo (first-party) is to check for the
+presence of ` + "`BUILD_WORKSPACE_DIRECTORY`" + ` in the environment, then change the working
+directory of the process. You'd typically do this at the very beginning of the program execution.
 `,
-		GroupID: "common",
+		GroupID:               "common",
+		DisableFlagsInUseLine: true,
 		RunE: interceptors.Run(
 			[]interceptors.Interceptor{
 				flags.FlagsInterceptor(streams),
