3c83aa9798b9f82c4ab2413a276d1b7917569c3a
Greg Magolan <gmagolan@gmail.com>
2022-11-10 16:55:05 -0800

    cli: hoist outputs command to be an oss build-in (#729)
    GitOrigin-RevId: f56651968ae85e18fc4200736d2654dc9b8e56ec

---
+++ pkg/aspect/outputs/outputs.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package outputs
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/ioutils"
+)
+
+type Outputs struct {
+	ioutils.Streams
+}
+
+func New(streams ioutils.Streams) *Outputs {
+	return &Outputs{
+		Streams: streams,
+	}
+}
+
+func (v *Outputs) Run(_ context.Context, _ *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("a label is required as the first argument to aspect outputs")
+	}
+	query := args[0]
+	var mnemonicFilter string
+	if len(args) > 1 {
+		mnemonicFilter = args[1]
+	}
+	bzl, err := bazel.FindFromWd()
+	if err != nil {
+		return err
+	}
+	agc, err := bzl.AQuery(query)
+	if err != nil {
+		return err
+	}
+	outs := bazel.ParseOutputs(agc)
+	for _, a := range outs {
+		if len(mnemonicFilter) > 0 {
+			if a.Mnemonic == mnemonicFilter {
+				fmt.Printf("%s\n", a.Path)
+			}
+		} else {
+			fmt.Printf("%s %s\n", a.Mnemonic, a.Path)
+		}
+	}
+	return nil
+}
