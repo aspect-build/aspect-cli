f3e9522fe3793191ecd7224e0f0c2f89e72a5d3b
Alex Eagle <alex@aspect.dev>
2022-11-12 08:09:32 -0800

    chore: add coverage command (#748)
    GitOrigin-RevId: 9210e09bd213e8461b1fe69b452a49c3a36e134c

---
+++ pkg/aspect/coverage/coverage.go
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package coverage
+
+import (
+	"fmt"
+
+	"aspect.build/cli/pkg/aspecterrors"
+	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/ioutils"
+	"aspect.build/cli/pkg/plugin/system/bep"
+)
+
+type Coverage struct {
+	ioutils.Streams
+	bzl bazel.Bazel
+}
+
+func New(streams ioutils.Streams, bzl bazel.Bazel) *Coverage {
+	return &Coverage{
+		Streams: streams,
+		bzl:     bzl,
+	}
+}
+
+func (t *Coverage) Run(args []string, besBackend bep.BESBackend) (exitErr error) {
+	besBackendFlag := fmt.Sprintf("--bes_backend=%s", besBackend.Addr())
+	bazelCmd := []string{"coverage", besBackendFlag}
+	bazelCmd = append(bazelCmd, args...)
+
+	exitCode, bazelErr := t.bzl.RunCommand(t.Streams, bazelCmd...)
+
+	// Process the subscribers errors before the Bazel one.
+	subscriberErrors := besBackend.Errors()
+	if len(subscriberErrors) > 0 {
+		for _, err := range subscriberErrors {
+			fmt.Fprintf(t.Streams.Stderr, "Error: failed to run coverage command: %v\n", err)
+		}
+		exitCode = 1
+	}
+
+	if exitCode != 0 {
+		err := &aspecterrors.ExitError{ExitCode: exitCode}
+		if bazelErr != nil {
+			err.Err = bazelErr
+		}
+		return err
+	}
+
+	return nil
+}
