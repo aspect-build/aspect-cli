aspect-build/aspect-cli

commit b0dc7a5d1357f74e43345ab9e7d527f3de49446b

fix: flag parsing (#993)
- remove all hidden flags
- only allow flags that we want to show explicitly
- passthrough flags that are unknown to aspect-cli
- throw on any unknown flags that start with --aspect:

benchmarks for new new `--nobatch` flag;

| Command | Mean [s] | Min [s] | Max [s] | Relative |
|:---|---:|---:|---:|---:|
| `bazel help flags-as-proto` | 1.085 ± 0.219 | 0.911 | 1.682 | 30.99 ±
7.95 |
| `bazel --nobatch --ignore_all_rc_files help flags-as-proto` | 0.035 ±
0.006 | 0.030 | 0.053 | 1.00 |

GitOrigin-RevId: 07de5a1d28126725a04d512255da80ccc3fccc49

cmd/aspect/help/BUILD.bazel
@@ -1,0 +1,20 @@
+load("@io_bazel_rules_go//go:def.bzl", "go_library")
+
+go_library(
+    name = "help",
+    srcs = [
+        "flags_as_proto.go",
+        "help.go",
+    ],
+    importpath = "aspect.build/cli/cmd/aspect/help",
+    visibility = ["//visibility:public"],
+    deps = [
+        "//docs/help/topics",
+        "//pkg/aspect/root/flags",
+        "//pkg/aspecterrors",
+        "//pkg/bazel",
+        "//pkg/interceptors",
+        "//pkg/ioutils",
+        "@com_github_spf13_cobra//:cobra",
+    ],
+)

cmd/aspect/help/help.go
@@ -1,0 +1,55 @@
+package help
+
+import (
+	"aspect.build/cli/docs/help/topics"
+	"github.com/spf13/cobra"
+)
+
+func NewCmd() *cobra.Command {
+	cmd := cobra.Command{
+		Use:  "help <command>",
+		Args: cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			if len(args) == 1 {
+				name := args[0]
+
+				for _, cmd := range cmd.Root().Commands() {
+					if cmd.Name() == name {
+						return cmd.Help()
+					}
+				}
+
+			}
+
+			return cmd.Help()
+		},
+	}
+
+	cmd.AddCommand(NewDefaultFlagsAsProtoCmd())
+
+	// ### "Additional help topic commands" which are not runnable
+	// https://pkg.go.dev/github.com/spf13/cobra#Command.IsAdditionalHelpTopicCommand
+	cmd.AddCommand(&cobra.Command{
+		Use:   "target-syntax",
+		Short: "Explains the syntax for specifying targets.",
+		Long:  topics.MustAssetString("target-syntax.md"),
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "info-keys",
+		Short: "Displays a list of keys used by the info command.",
+		Long:  topics.MustAssetString("info-keys.md"),
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "tags",
+		Short: "Conventions for tags which are special.",
+		Long:  topics.MustAssetString("tags.md"),
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "plugins",
+		Short: "How to extend aspect with a custom plugin.",
+		Long:  topics.MustAssetString("plugins.md"),
+	})
+
+	return &cmd
+}

cmd/aspect/root/BUILD.bazel
@@ -18,6 +18,7 @@ go_library(
         "//cmd/aspect/docs",
         "//cmd/aspect/dump",
         "//cmd/aspect/fetch",
+        "//cmd/aspect/help",
         "//cmd/aspect/info",
         "//cmd/aspect/license",
         "//cmd/aspect/mobileinstall",
@@ -33,7 +34,6 @@ go_library(
         "//cmd/aspect/sync",
         "//cmd/aspect/test",
         "//cmd/aspect/version",
-        "//docs/help/topics",
         "//pkg/aspect/root/flags",
         "//pkg/ioutils",
         "//pkg/plugin/system",

cmd/aspect/root/root.go
@@ -36,6 +36,7 @@ import (
 	"aspect.build/cli/cmd/aspect/docs"
 	"aspect.build/cli/cmd/aspect/dump"
 	"aspect.build/cli/cmd/aspect/fetch"
+	"aspect.build/cli/cmd/aspect/help"
 	"aspect.build/cli/cmd/aspect/info"
 	"aspect.build/cli/cmd/aspect/license"
 	"aspect.build/cli/cmd/aspect/mobileinstall"
@@ -51,7 +52,6 @@ import (
 	"aspect.build/cli/cmd/aspect/sync"
 	"aspect.build/cli/cmd/aspect/test"
 	"aspect.build/cli/cmd/aspect/version"
-	"aspect.build/cli/docs/help/topics"
 	"aspect.build/cli/pkg/aspect/root/flags"
 	"aspect.build/cli/pkg/ioutils"
 	"aspect.build/cli/pkg/plugin/system"
@@ -123,6 +123,8 @@ func NewCmd(
 	cmd.AddCommand(test.NewDefaultCmd(pluginSystem))
 	cmd.AddCommand(version.NewDefaultCmd())
 	cmd.AddCommand(outputs.NewDefaultCmd())
+	cmd.SetHelpCommand(help.NewCmd())
+
 	if !buildinfo.Current().IsAspectPro {
 		// Aspect CLI only commands
 		cmd.AddCommand(license.NewDefaultCmd())
@@ -132,27 +134,5 @@ func NewCmd(
 		cmd.AddCommand(support.NewDefaultCmd())
 	}
 
-	// ### "Additional help topic commands" which are not runnable
-	// https://pkg.go.dev/github.com/spf13/cobra#Command.IsAdditionalHelpTopicCommand
-	cmd.AddCommand(&cobra.Command{
-		Use:   "target-syntax",
-		Short: "Explains the syntax for specifying targets.",
-		Long:  topics.MustAssetString("target-syntax.md"),
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "info-keys",
-		Short: "Displays a list of keys used by the info command.",
-		Long:  topics.MustAssetString("info-keys.md"),
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "tags",
-		Short: "Conventions for tags which are special.",
-		Long:  topics.MustAssetString("tags.md"),
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "plugins",
-		Short: "How to extend aspect with a custom plugin.",
-		Long:  topics.MustAssetString("plugins.md"),
-	})
 	return cmd
 }

integration_tests/aspect/help_test.bats
@@ -1,0 +1,44 @@
+load "common.bats"
+
+setup() {
+    touch WORKSPACE
+}
+
+@test '--help flag should work' {
+    run aspect --help
+    assert_output --partial "Common Bazel Commands:" "Commands only in Aspect CLI:"
+}
+
+@test 'help command should work' {
+    run aspect help build
+    assert_output --partial "Performs a build on the specified targets, producing their default outputs." "Documentation: <https://bazel.build/run/build#bazel-build>"
+    assert_output --partial "aspect build <target patterns> [flags]"
+}
+
+@test 'aspect help' {
+    run aspect help
+    assert_output --partial "Available Commands:" "flags-as-proto"
+}
+
+@test 'aspect help target-syntax' {
+    run aspect help target-syntax
+    assert_output --partial "Target pattern syntax"
+}
+
+@test 'aspect help info-keys' {
+    run aspect help info-keys 
+    assert_output --partial "bazel-bin"
+}
+
+
+@test 'aspect help tags' {
+    run aspect help tags
+    assert_output --partial "exclusive"
+}
+
+@test 'aspect help flags-as-proto' {
+    run --separate-stderr aspect help flags-as-proto
+    aspect_output=$output
+    run --separate-stderr $BAZEL_BINARY --nosystem_rc --nohome_rc help flags-as-proto
+    assert_equal "$output" "$aspect_output"
+}

integration_tests/BUILD.bazel
@@ -1,0 +1,23 @@
+load("@bazel_skylib//:bzl_library.bzl", "bzl_library")
+
+exports_files(["runner.sh"])
+
+bzl_library(
+    name = "bats_deps",
+    srcs = ["bats_deps.bzl"],
+    visibility = ["//visibility:public"],
+    deps = ["@bazel_tools//tools/build_defs/repo:http.bzl"],
+)
+
+bzl_library(
+    name = "bazel_binary",
+    srcs = ["bazel_binary.bzl"],
+    visibility = ["//visibility:public"],
+    deps = ["@aspect_bazel_lib//lib:repo_utils"],
+)
+
+bzl_library(
+    name = "bats",
+    srcs = ["bats.bzl"],
+    visibility = ["//visibility:public"],
+)

integration_tests/aspect/BUILD.bazel
@@ -1,12 +1,36 @@
-sh_test(
-    name = "help_test",
-    srcs = ["help_test.sh"],
-    data = ["//cmd/aspect"],
+load("//cli/core/integration_tests:bats.bzl", "bats_test")
+
+exports_files(["common.bats"])
+
+# common env
+env = {
+    "BAZEL_BINARY": "$(location @bazel_6_0_0rc4//:bazel)",
+}
+
+# common data
+data = [
+    "common.bats",
+    "//cmd/aspect",
+    "@bazel_6_0_0rc4//:bazel",
+]
+
+bats_test(
+    name = "flags_test",
+    srcs = ["flags_test.bats"],
+    data = data,
+    env = env,
 )
 
-sh_test(
+bats_test(
+    name = "help_test",
+    srcs = ["help_test.bats"],
+    data = data,
+    env = env,
+)
+
+bats_test(
     name = "version_test",
-    srcs = ["version_test.sh"],
-    data = ["//cmd/aspect"],
-    tags = ["manual"],
+    srcs = ["version_test.bats"],
+    data = data,
+    env = env,
 )

integration_tests/aspect/info/BUILD.bazel
@@ -1,6 +1,20 @@
-sh_test(
-    name = "can_pass_arguments",
-    srcs = ["can_pass_arguments.sh"],
-    data = ["//cmd/aspect"],
-    tags = ["manual"],
+load("//cli/core/integration_tests:bats.bzl", "bats_test")
+
+# common env
+env = {
+    "BAZEL_BINARY": "$(location @bazel_6_0_0rc4//:bazel)",
+}
+
+# common data
+data = [
+    "//cli/core/integration_tests/aspect:common.bats",
+    "//cmd/aspect",
+    "@bazel_6_0_0rc4//:bazel",
+]
+
+bats_test(
+    name = "info_test",
+    srcs = ["info_test.bats"],
+    data = data,
+    env = env,
 )

commit 4b05859cf38285fba7748975e6f23e5a8dbdfe34

fix(cli): aspect help with no args should display root help (#1196)
This regressed in #993 when `aspect help flags-as-proto` support was
added.

Output is now correct again:

```
$ aspect help
Aspect CLI Pro is a better frontend for running bazel

Usage:
  aspect [command]

Common Bazel Commands:
  build              Build the specified targets
  cquery             Query the dependency graph, honoring configuration flags
  run                Build a single target and run it with the given arguments
  test               Build the specified targets and run all test targets among them
  version            Print the versions of Aspect CLI and Bazel

Commands only in Aspect CLI:
  configure          Update BUILD files for JavaScript, TypeScript, Go and Protobuf
  docs               Open documentation in the browser
  init               Create a new Bazel workspace
  outputs            Print paths to declared output files
  print              Print syntax elements from BUILD files
  support            Interactive, human-escalated support for Bazel problems

Custom Commands from Plugins:

Other Bazel Built-in Commands:
  analyze-profile    Analyze build profile data
  aquery             Query the action graph
  canonicalize-flags Present a list of bazel options in a canonical form
  clean              Remove the output tree
  coverage           Same as 'test', but also generates a code coverage report.
  fetch              Fetch external repositories that are prerequisites to the targets
  info               Display runtime info about the bazel server
  license            Prints the license of this software.
  query              Query the dependency graph, ignoring configuration flags
  shutdown           Stop the bazel server

Additional Commands:
  completion         Generate the autocompletion script for the specified shell
  help

Flags:

cmd/aspect/help/BUILD.bazel
@@ -9,7 +9,6 @@ go_library(
     importpath = "aspect.build/cli/cmd/aspect/help",
     visibility = ["//visibility:public"],
     deps = [
-        "//docs/help/topics",
         "//pkg/aspect/root/flags",
         "//pkg/aspecterrors",
         "//pkg/bazel",

cmd/aspect/help/help.go
@@ -3,25 +3,19 @@ package help
 import (
 	"fmt"
 
-	"aspect.build/cli/docs/help/topics"
 	"github.com/spf13/cobra"
 )
 
-func mustReadFile(f string) string {
-	result, err := topics.Content.ReadFile(f)
-	if err != nil {
-		panic(fmt.Errorf("Internal error: embed data was not readable: %w", err))
-	}
-	return string(result)
-}
-
 func NewCmd() *cobra.Command {
 	cmd := cobra.Command{
-		Use:  "help <command>",
-		Args: cobra.MaximumNArgs(1),
+		Use: "help <command>",
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			if len(args) == 1 {
+			if len(args) == 0 {
+				// `aspect help` with no args should display root command help string
+				// (same as if you run `aspect` with no args)
+				return cmd.Root().Help()
+			} else if len(args) == 1 {
 				name := args[0]
 
 				for _, cmd := range cmd.Root().Commands() {
@@ -30,31 +24,14 @@ func NewCmd() *cobra.Command {
 					}
 				}
 
+				return fmt.Errorf("%s is not a known command", name)
 			}
 
-			return cmd.Help()
+			return fmt.Errorf("You must specify exactly one command")
 		},
 	}
 
 	cmd.AddCommand(NewDefaultFlagsAsProtoCmd())
-
-	// ### "Additional help topic commands" which are not runnable
-	// https://pkg.go.dev/github.com/spf13/cobra#Command.IsAdditionalHelpTopicCommand
-	cmd.AddCommand(&cobra.Command{
-		Use:   "target-syntax",
-		Short: "Explains the syntax for specifying targets.",
-		Long:  mustReadFile("target-syntax.md"),
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "info-keys",
-		Short: "Displays a list of keys used by the info command.",
-		Long:  mustReadFile("info-keys.md"),
-	})
-	cmd.AddCommand(&cobra.Command{
-		Use:   "tags",
-		Short: "Conventions for tags which are special.",
-		Long:  mustReadFile("tags.md"),
-	})
 
 	return &cmd
 }

cmd/aspect/root/BUILD.bazel
@@ -35,6 +35,7 @@ go_library(
         "//cmd/aspect/sync",
         "//cmd/aspect/test",
         "//cmd/aspect/version",
+        "//docs/help/topics",
         "//pkg/aspect/root/flags",
         "//pkg/ioutils",
         "//pkg/plugin/system",

cmd/aspect/root/root.go
@@ -53,6 +53,7 @@ import (
 	"aspect.build/cli/cmd/aspect/sync"
 	"aspect.build/cli/cmd/aspect/test"
 	"aspect.build/cli/cmd/aspect/version"
+	"aspect.build/cli/docs/help/topics"
 	"aspect.build/cli/pkg/aspect/root/flags"
 	"aspect.build/cli/pkg/ioutils"
 	"aspect.build/cli/pkg/plugin/system"
@@ -136,5 +137,31 @@ func NewCmd(
 		cmd.AddCommand(support.NewDefaultCmd())
 	}
 
+	// ### "Additional help topic commands" which are not runnable
+	// https://pkg.go.dev/github.com/spf13/cobra#Command.IsAdditionalHelpTopicCommand
+	cmd.AddCommand(&cobra.Command{
+		Use:   "target-syntax",
+		Short: "Explains the syntax for specifying targets.",
+		Long:  mustReadFile("target-syntax.md"),
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "info-keys",
+		Short: "Displays a list of keys used by the info command.",
+		Long:  mustReadFile("info-keys.md"),
+	})
+	cmd.AddCommand(&cobra.Command{
+		Use:   "tags",
+		Short: "Conventions for tags which are special.",
+		Long:  mustReadFile("tags.md"),
+	})
+
 	return cmd
+}
+
+func mustReadFile(f string) string {
+	result, err := topics.Content.ReadFile(f)
+	if err != nil {
+		panic(fmt.Errorf("Internal error: embed data was not readable: %w", err))
+	}
+	return string(result)
 }

integration_tests/aspect/help_test.bats
@@ -15,9 +15,14 @@ setup() {
     assert_output --partial "aspect build <target patterns> [flags]"
 }
 
-@test 'aspect help' {
+@test 'aspect with no args should display root help' {
+    run aspect
+    assert_output --partial "Aspect CLI is a better frontend for running bazel" "Common Bazel Commands" "Commands only in Aspect CLI" "Other Bazel Built-in Commands" "Additional Commands" "Additional help topics"
+}
+
+@test 'aspect help with no args should display root help' {
     run aspect help
-    assert_output --partial "Available Commands:" "flags-as-proto"
+    assert_output --partial "Aspect CLI is a better frontend for running bazel" "Common Bazel Commands" "Commands only in Aspect CLI" "Other Bazel Built-in Commands" "Additional Commands" "Additional help topics"
 }
 
 @test 'aspect help target-syntax' {
@@ -30,7 +35,6 @@ setup() {
     assert_output --partial "bazel-bin"
 }
 
-
 @test 'aspect help tags' {
     run aspect help tags
     assert_output --partial "exclusive"
@@ -39,6 +43,6 @@ setup() {
 @test 'aspect help flags-as-proto' {
     run --separate-stderr aspect help flags-as-proto
     aspect_output=$output
-    run --separate-stderr $BAZEL_BINARY --nosystem_rc --nohome_rc help flags-as-proto
+    run --separate-stderr "$BAZEL_BINARY" --nosystem_rc --nohome_rc help flags-as-proto
     assert_equal "$output" "$aspect_output"
 }