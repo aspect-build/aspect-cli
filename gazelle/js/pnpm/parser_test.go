1fbabd299ed1e61b57fe08f5979e581ad26e5dff
Jason Bedard <jason@aspect.dev>
2023-04-11 11:00:38 -0700

    gazelle: add support for pnpm lockfile v6 (#1879)
    Fix aspect-build/aspect-cli#417
    
    GitOrigin-RevId: ffe7530c75fffd2b2a871439eed92cc1634f63a8

--- gazelle/js/pnpm/parser_test.go
+++ gazelle/js/pnpm/parser_test.go
@@ -5,15 +5,46 @@ import (
 )
 
 func TestPnpmLockParseDependencies(t *testing.T) {
+	t.Run("lockfile version", func(t *testing.T) {
+		v, e := parsePnpmLockVersion([]byte("lockfileVersion: 5.4"))
+		if e != nil {
+			t.Error(e)
+		} else if v != "5.4" {
+			t.Error("Failed to parse lockfile version 5.4")
+		}
+
+		v, e = parsePnpmLockVersion([]byte("lockfileVersion: '6.0'"))
+		if e != nil {
+			t.Error(e)
+		} else if v != "6.0" {
+			t.Error("Failed to parse lockfile version 6.0")
+		}
+	})
+
 	t.Run("empty lock file", func(t *testing.T) {
-		emptyLock := parsePnpmLockDependencies([]byte(""))
+		emptyLock, err := parsePnpmLockDependencies([]byte(""))
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
 		if emptyLock == nil {
 			t.Error("Empty lock file not parsed")
 		}
 	})
 
-	t.Run("basic deps", func(t *testing.T) {
-		basic := parsePnpmLockDependencies([]byte(`
+	t.Run("unsupported version", func(t *testing.T) {
+		_, err := parsePnpmLockDependencies([]byte("lockfileVersion: 4.0"))
+		if err == nil {
+			t.Error("Expected error for unsupported version")
+		}
+
+		_, err2 := parsePnpmLockDependencies([]byte("lockfileVersion: '4.0'"))
+		if err2 == nil {
+			t.Error("Expected error for unsupported version")
+		}
+	})
+
+	t.Run("basic deps (lockfile v5)", func(t *testing.T) {
+		basic, err := parsePnpmLockDependencies([]byte(`
 lockfileVersion: 5.4
 
 specifiers:
@@ -31,6 +62,10 @@ peerDependencies:
   jquery: 3.6.1
 `))
 
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
+
 		if len(basic) != 1 || basic["."] == nil {
 			t.Error("Simple deps parse error. Expected only '.' workspace, found ", len(basic))
 		}
@@ -38,10 +73,85 @@ peerDependencies:
 		if len(basic["."]) != 3 {
 			t.Error("Simple deps parse error. Expected 3 deps in 1 workspace entry, found ", len(basic["."]))
 		}
+
+		if basic["."]["jquery"] != "3.6.1" {
+			t.Errorf("Simple deps parse error. Expected 2.0.2 version for @aspect-test/c, found %q", basic["."]["@aspect-test/c"])
+		}
 	})
 
-	t.Run("basic deps in single project workspace", func(t *testing.T) {
-		basic := parsePnpmLockDependencies([]byte(`
+	t.Run("basic deps (lockfile v6)", func(t *testing.T) {
+		basic, err := parsePnpmLockDependencies([]byte(`
+lockfileVersion: '6.0'
+
+dependencies:
+  '@aspect-test/a':
+    specifier: 5.0.2
+    version: 5.0.2
+  jquery:
+    specifier: 3.6.1
+    version: 3.6.1
+
+devDependencies:
+  '@aspect-test/c':
+    specifier: 2.0.2
+    version: 2.0.2
+`))
+
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
+
+		if len(basic) != 1 || basic["."] == nil {
+			t.Error("Simple deps parse error. Expected only '.' workspace, found ", len(basic))
+		}
+
+		if len(basic["."]) != 3 {
+			t.Error("Simple deps parse error. Expected 3 deps in 1 workspace entry, found ", len(basic["."]))
+		}
+
+		if basic["."]["jquery"] != "3.6.1" {
+			t.Errorf("Simple deps parse error. Expected 2.0.2 version for @aspect-test/c, found %q", basic["."]["@aspect-test/c"])
+		}
+	})
+
+	t.Run("basic deps in single project workspace (lockfile v6)", func(t *testing.T) {
+		basic, err := parsePnpmLockDependencies([]byte(`
+lockfileVersion: '6.0'
+
+importers:
+  .:
+    dependencies:
+      '@aspect-test/a':
+        specifier: 5.0.2
+        version: 5.0.2
+      jquery:
+        specifier: 3.6.1
+        version: 3.6.1
+    devDependencies:
+      '@aspect-test/c':
+        specifier: ^2.0.2
+        version: 2.0.2
+`))
+
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
+
+		if len(basic) != 1 || basic["."] == nil {
+			t.Error("Simple deps parse error. Expected only '.' workspace, found ", len(basic))
+		}
+
+		if len(basic["."]) != 3 {
+			t.Error("Simple deps parse error. Expected 3 deps in 1 workspace entry, found ", len(basic["."]))
+		}
+
+		if basic["."]["jquery"] != "3.6.1" {
+			t.Errorf("Simple deps parse error. Expected 2.0.2 version for @aspect-test/c, found %q", basic["."]["@aspect-test/c"])
+		}
+	})
+
+	t.Run("basic deps in single project workspace (lockfile v5)", func(t *testing.T) {
+		basic, err := parsePnpmLockDependencies([]byte(`
 lockfileVersion: 5.4
 
 importers:
@@ -57,6 +167,10 @@ importers:
       jquery: 3.6.1
 `))
 
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
+
 		if len(basic) != 1 || basic["."] == nil {
 			t.Error("Simple deps parse error. Expected only '.' workspace, found ", len(basic))
 		}
@@ -64,12 +178,20 @@ importers:
 		if len(basic["."]) != 3 {
 			t.Error("Simple deps parse error. Expected 3 deps in 1 workspace entry, found ", len(basic["."]))
 		}
+
+		if basic["."]["jquery"] != "3.6.1" {
+			t.Errorf("Simple deps parse error. Expected 2.0.2 version for @aspect-test/c, found %q", basic["."]["@aspect-test/c"])
+		}
 	})
 
 	t.Run("no deps property", func(t *testing.T) {
-		empty := parsePnpmLockDependencies([]byte(`
+		empty, err := parsePnpmLockDependencies([]byte(`
 lockfileVersion: 5.4
 `))
+
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
 
 		if len(empty) != 1 || len(empty["."]) != 0 {
 			t.Error("No deps parse error: ", empty)
@@ -77,7 +199,7 @@ lockfileVersion: 5.4
 	})
 
 	t.Run("workspace deps", func(t *testing.T) {
-		wksps := parsePnpmLockDependencies([]byte(`
+		wksps, err := parsePnpmLockDependencies([]byte(`
 lockfileVersion: 5.4
 importers:
   .:
@@ -95,6 +217,10 @@ importers:
 packages:
   /@aspect-test/c/2.0.2:
 `))
+
+		if err != nil {
+			t.Error("Parse failure: ", err)
+		}
 
 		if len(wksps) != 3 || wksps["."] == nil || wksps["gazelle/ts/tests/simple_json_import"] == nil || wksps["infrastructure/cdn"] == nil {
 			t.Error("expected 3 importers, found: ", len(wksps))
