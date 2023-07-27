2be10c362344cbba83e5746b03249eaafe6d98af
Matt Mackay <matt@aspect.dev>
2023-07-27 17:38:08 -0400

    fix: enable running install tests on MacOS (#2989)
    Removes the remaining linux target only on the bats tests so we can run
    them locally.
    
    Follow up to #2971
    Blocking #2969
    
    ---
    
    ### Type of change
    
    - Refactor (a code change that neither fixes a bug or adds a new
    feature)
    
    ### Test plan
    
    - Covered by existing test cases
    
    GitOrigin-RevId: 6c68fe89bbc0515a570ffa9413db000953de4afd

--- integration_tests/runner.sh
+++ integration_tests/runner.sh
@@ -2,7 +2,7 @@ IFS=':' read -ra LIBS <<< "$BATS_LIB_PATH"
 
 NEW_LIBS=()
 for RAW_LIB_PATH in "${LIBS[@]}"; do
-    NEW_PATH=$(cd $RAW_LIB_PATH && pwd)
+    NEW_PATH=$(cd "${RAW_LIB_PATH}" && pwd)
     NEW_LIBS+=("$NEW_PATH")
 done
 
@@ -13,4 +13,4 @@ export BATS_LIB_PATH=$(
 export BATS_TEST_TIMEOUT="$TEST_TIMEOUT"
 export BATS_TMPDIR="$TEST_TMPDIR"
 
-exec $BIN $@
+exec "${BIN}" "$@"
