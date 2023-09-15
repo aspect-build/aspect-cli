3cdde03d4e2de88787ae4bf0c793ca6395e0166d
Greg Magolan <gmagolan@gmail.com>
2023-09-15 15:20:39 -0700

    feat: add --aspect:disable_plugins flag to Aspect CLI (#3507)
    Since AbleAI is now going to be make `bazel` calls outside of our usual
    Workflows task.
    
    ---
    
    ### Type of change
    
    - New feature or functionality (change which adds functionality)
    
    **For changes visible to end-users**
    
    - Suggested release notes are provided below:
    
    Aspect CLI now has a flag named `--aspect:disable_plugins` that disables
    the plugins system so that plugins are not started even if configured in
    the Aspect CLI config.yaml.
    
    ### Test plan
    
    - Manual testing; please provide instructions so we can reproduce:
    
    Manually tested this. We need to add a plugins system bats test soon as
    it is missing but no time atm since this change is blocking the AbleAI
    trial.
    
    GitOrigin-RevId: 42067a1ca1b7ccdc224b380e13ec132c29133bdf

--- pkg/aspect/root/flags/global.go
+++ pkg/aspect/root/flags/global.go
@@ -34,6 +34,9 @@ func AddGlobalFlags(cmd *cobra.Command, defaultInteractive bool) {
 	cmd.PersistentFlags().Bool(AspectForceBesBackendFlagName, false, "Force the creation of a BES backend even if there are no plugins loaded")
 	cmd.PersistentFlags().MarkHidden(AspectForceBesBackendFlagName)
 
+	cmd.PersistentFlags().Bool(AspectDisablePluginsFlagName, false, "Disable the plugin system. This prevents Aspect CLI for starting any plugins.")
+	cmd.PersistentFlags().MarkHidden(AspectDisablePluginsFlagName)
+
 	RegisterNoableBool(cmd.PersistentFlags(), AspectSystemConfigFlagName, true, "Whether or not to look for the system config file at /etc/aspect/cli/config.yaml")
 	cmd.PersistentFlags().MarkHidden(AspectSystemConfigFlagName)
 	cmd.PersistentFlags().MarkHidden(NoFlagName(AspectSystemConfigFlagName))
