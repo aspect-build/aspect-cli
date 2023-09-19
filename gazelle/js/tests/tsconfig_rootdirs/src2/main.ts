73727b03bf5ce37cd70260cf9a67bf4e419f96c7
Jason Bedard <jason@aspect.dev>
2023-09-19 11:04:47 -0700

    fix(gazelle): fix tsconfig baseUrl/paths mapping (#3524)
    The tsconfig `paths` were not being treated relative to the `tsconfig`
    `baseUrl`. The tests should be the exact cases described at
    https://www.typescriptlang.org/docs/handbook/module-resolution.html#path-mapping
    
    ---
    
    ### Type of change
    
    - Bug fix (change which fixes an issue)
    
    ### Test plan
    
    - New test cases added
    
    GitOrigin-RevId: bfcad695000fef0ae219016d4dc3738b867012f3

--- gazelle/js/tests/tsconfig_rootdirs/src2/main.ts
+++ gazelle/js/tests/tsconfig_rootdirs/src2/main.ts
@@ -1,3 +1,4 @@
-import lang from './lang/strings';
+import lang from './lang/strings'; // with relative ./
+import lang2 from 'lang/strings'; // without relative ./
 
-console.log(lang.hello);
+console.log(lang.hello, lang2.hello);
