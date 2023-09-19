d8a9c6dc036dbfe63c5007617357f39fffe6abbf
Jason Bedard <jason@aspect.dev>
2023-09-19 11:04:47 -0700

    fix(gazelle): fix tsconfig baseUrl/paths mapping (#3524)
    The tsconfig `paths` were not being treated relative to the `tsconfig`
    `baseUrl`. The tests should be the exact cases described at
    https://www.typescriptlang.org/docs/handbook/module-resolution.html#path-mapping
    
    ---
    
    ### Type of change
    
    - Bug fix (change which fixes an issue)
    
    ### Test plan
    
    - New test cases added
    
    GitOrigin-RevId: bfcad695000fef0ae219016d4dc3738b867012f3

---
+++ gazelle/js/tests/tsconfig_paths/overwriter2/src/main.ts
@@ -0,0 +1,6 @@
+import { A } from 'new-star/a';
+import { B } from 'new-star/b';
+import { A as AliasA } from 'new-alias-a';
+import { C } from 'src/overlib/o';
+
+console.log(A, B, AliasA, C);
--- gazelle/js/tests/tsconfig_paths/src/main.ts
+++ gazelle/js/tests/tsconfig_paths/src/main.ts
@@ -4,6 +4,18 @@ import { A as AliasA } from 'alias-a';
 import { A as RootDotA } from 'lib/a';
 import { C1 } from 'multi-c/c1';
 import { C2 } from 'multi-c/c2';
-import { F } from 'f1';
+import { F as Fallback1 } from 'f1';
+import { F2 as FallbackSubdir } from 'f2/a';
+import { F2 as FallbackRoot } from 'fallback/f2/a';
 
-console.log(A, B, AliasA, RootDotA, C1, C2, F);
+console.log(
+    A,
+    B,
+    AliasA,
+    RootDotA,
+    C1,
+    C2,
+    Fallback1,
+    FallbackSubdir,
+    FallbackRoot
+);
