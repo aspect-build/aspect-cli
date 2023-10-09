3266607b593c18779f54fd2104106a2c2409ef89
Red Daly <reddaly@x.team>
2023-10-09 04:49:23 +0000

    copy gazelle/kotlin/tests/simple_file test

---
+++ gazelle/kotlin/tests/simple_file2/lib.kt
@@ -0,0 +1,5 @@
+package gazelle.kotlin.tests.simple_file2// Hello World Program
+
+fun hello() {
+    println("Hello world!")
+}
---
+++ gazelle/kotlin/tests/simple_file2/libs.kts
@@ -0,0 +1,7 @@
+package gazelle.kotlin.tests.simple_file2
+
+// Hello World Program
+
+fun hello() {
+    println("Hello world!")
+}
