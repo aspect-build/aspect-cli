76bde0a257635f451ce4f95d1f844316d149307f
Matt Mackay <matt@aspect.dev>
2023-11-28 20:33:49 -0500

    feat: add 'config' command to CLI (#3905)
    Adds the missing `config` command to the CLI.
    
    It should be hidden, but when it's set to hidden, Cobra is seemingly
    confused and suggests that we want to run `configure` instead.
    
    ---
    
    ### Type of change
    
    - New feature or functionality (change which adds functionality)
    
    **For changes visible to end-users**
    
    - Relevant documentation has been updated
    - Suggested release notes are provided below:
    
    Add the Bazel `config` command to the Aspect CLI.
    
    ### Test plan
    
    - Manual testing; please provide instructions so we can reproduce:
    
    Run the built binary, can now run the `config` command.
    
    GitOrigin-RevId: 9098c884fe3949ec31d119207f63f45c6c104383

---
+++ cmd/aspect/config/config.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config
+
+import (
+	"github.com/spf13/cobra"
+
+	"aspect.build/cli/pkg/aspect/config"
+	"aspect.build/cli/pkg/aspect/root/flags"
+	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/interceptors"
+	"aspect.build/cli/pkg/ioutils"
+)
+
+func NewDefaultCmd() *cobra.Command {
+	return NewCmd(ioutils.DefaultStreams, bazel.WorkspaceFromWd)
+}
+
+func NewCmd(streams ioutils.Streams, bzl bazel.Bazel) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "config",
+		Short:   "Displays details of configurations.",
+		GroupID: "built-in",
+		RunE: interceptors.Run(
+			[]interceptors.Interceptor{
+				flags.FlagsInterceptor(streams),
+			},
+			config.New(streams, bzl).Run,
+		),
+	}
+	return cmd
+}
