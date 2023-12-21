7f1960bd2dad36361e7c02aa0ad579272b656cc7
Greg Magolan <greg@aspect.dev>
2023-12-21 11:12:18 -0800

    chore: bazel run //:format

--- pkg/aspect/outputs/hash.go
+++ pkg/aspect/outputs/hash.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package outputs
 
 import (
@@ -11,8 +27,8 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/alphadose/haxmap"
 	"aspect.build/cli/pkg/bazel"
+	"github.com/alphadose/haxmap"
 	"github.com/rogpeppe/go-internal/dirhash"
 	concurrently "github.com/tejzpr/ordered-concurrently/v3"
 	"github.com/twmb/murmur3"
