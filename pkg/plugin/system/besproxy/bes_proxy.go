c734dacd513b4f7376417afb174612ce3597218b
Greg Magolan <greg@aspect.dev>
2023-12-21 11:12:18 -0800

    chore: bazel run //:format

--- pkg/plugin/system/besproxy/bes_proxy.go
+++ pkg/plugin/system/besproxy/bes_proxy.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package besproxy
 
 import (
@@ -214,7 +230,7 @@ func MutateWorkflowsUri(f *buildeventstream.File, remoteCacheAddress string) boo
 			// bytestream:////some/path/on/the/local/filesystem/grpc/blobs/f9c5cd4a9f458cf3d801640f6f69eb7523bc590b92d4e7b7b9fa5c7ffa4813cb/194
 			// to,
 			// bytestream://10.2.0.110:8980/blobs/f9c5cd4a9f458cf3d801640f6f69eb7523bc590b92d4e7b7b9fa5c7ffa4813cb/194
-			t.Uri = r.ReplaceAllString(t.Uri, "bytestream://" + remoteCacheAddress + "/$1")
+			t.Uri = r.ReplaceAllString(t.Uri, "bytestream://"+remoteCacheAddress+"/$1")
 			return true
 		}
 
