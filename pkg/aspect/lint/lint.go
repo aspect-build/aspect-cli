9160b941c2901a4d5ceb9a1a89154aa1feed2422
Alex Eagle <alex@aspect.dev>
2024-03-07 15:44:18 -0800

    refactor: copybara overhaul (#4771)
    Make the transform simpler by removing special cases:
    
    - moving the "help topics" docs under cli/core, give up on publishing
    them on the public site for now.
    - move one import to a separate block so it doesn't get re-ordered after
    rewriting our go imports
    - express the transforms as silo -> OSS which is the way we think about
    it now. Originally when I wrote this, the direction was opposite since
    we were bringing the OSS repo into silo.
    - move the go_generated_sources helper
    
    Added a couple more copybara workflows that let me test locally and iron
    out the diffs.
    
    ---
    
    ### Type of change
    
    - Refactor (a code change that neither fixes a bug or adds a new
    feature)
    
    ### Test plan
    
    - Manual testing; please provide instructions so we can reproduce:
    
    Run a sync from this branch, it's totally clean with no manual fixups:
    https://github.com/aspect-build/aspect-cli/pull/651
    
    Also ran the two added workflows and checked in meld that the transforms
    line up in both directions.
    Did not test the import_pr workflow yet, will do after this lands.
    
    GitOrigin-RevId: bfb27b61d7c85691a776d1db852da03b5617ac36

--- pkg/aspect/lint/lint.go
+++ pkg/aspect/lint/lint.go
@@ -1,20 +1,4 @@
 /*
- * Copyright 2023 Aspect Build Systems, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
  * Copyright 2022 Aspect Build Systems, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -129,7 +113,7 @@ lint:
 	output, _ := cmd.Flags().GetString("output")
 
 	bazelCmd := []string{"build"}
-	bazelCmd = append(bazelCmd, cmd.Flags().Args()...)
+	bazelCmd = append(bazelCmd, args...)
 	bazelCmd = append(bazelCmd, fmt.Sprintf("--aspects=%s", strings.Join(linters, ",")))
 
 	outputGroups := []string{}
