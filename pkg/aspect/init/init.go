f2c15f9932db074cd2a00d837313d9840bc2d8e1
Alex Eagle <alex@aspect.dev>
2024-05-17 14:46:35 -0700

    refactor: use scaffold library to create init project (#5445)
    Removes the goembed folder that lived in silo.
    Allows us to update the scaffold independently of CLI releases.
    
    ---
    
    ### Changes are visible to end-users: yes
    
    - Searched for relevant documentation and updated as needed: yes
    - Breaking change (forces users to change their own code or config): no
    - Suggested release notes appear below: yes
    
    New workspace creation with `aspect init` is now more full-featured:
    - Uses an external repository which can be updated without CLI release
    - That repo stamps out an application with optional JS&TS and/or Python
    support; more languages to come
    - It optionally includes a stamping setup
    - It optionally installs rules_lint with a format target and linters for
    the selected languages
    
    ### Test plan
    
    - Covered by existing test cases
    
    GitOrigin-RevId: fe5da6545697edbe3546661094696a8854db5308

--- pkg/aspect/init/init.go
+++ pkg/aspect/init/init.go
@@ -14,34 +14,24 @@
  * limitations under the License.
  */
 
+// init is a thin adapter for Aspect CLI to the Scaffold CLI: https://hay-kot.github.io/scaffold/
 package init
 
 import (
 	"context"
-	"embed"
 	"fmt"
-	"io/fs"
 	"os"
-	"path"
-	"strings"
 
-	"aspect.build/cli/buildinfo"
-	"aspect.build/cli/docs/bazelrc"
-	"aspect.build/cli/pkg/bazel"
+	"aspect.build/cli/pkg/aspect/root/flags"
 	"aspect.build/cli/pkg/bazel/workspace"
 	"aspect.build/cli/pkg/ioutils"
 
-	semver "github.com/Masterminds/semver/v3"
-	"github.com/fatih/color"
-	"github.com/manifoldco/promptui"
+	"github.com/hay-kot/scaffold/app/commands"
+	"github.com/hay-kot/scaffold/app/core/engine"
+	"github.com/hay-kot/scaffold/app/scaffold"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
-)
-
-//go:embed template/** template/**/_*
-var Content embed.FS
-
-var (
-	boldCyan = color.New(color.FgCyan, color.Bold)
 )
 
 type Init struct {
@@ -54,45 +44,7 @@ func New(streams ioutils.Streams) *Init {
 	}
 }
 
-func (runner *Init) lookupAspectVersion() (string, error) {
-	aspectVersions, err := bazel.GetAspectVersions()
-	if err == nil && len(aspectVersions) > 0 {
-		return aspectVersions[0], nil
-	}
-
-	bi := *buildinfo.Current()
-
-	if !bi.HasRelease() {
-		return "", fmt.Errorf("Could not determine latest aspect release and current version is unstamped: %w", err)
-	}
-
-	// if we fail to get the latest release of Aspect CLI then fallback to stamping the current version
-	versionWithMeta, err := semver.NewVersion(bi.Release)
-	if err != nil {
-		return "", fmt.Errorf("Could not determine latest aspect release and failed to parse current version '%s' semver: %w", bi.Release, err)
-	}
-
-	// throw away metadata
-	version, err := versionWithMeta.SetMetadata("")
-	if err != nil {
-		return "", fmt.Errorf("Could not determine latest aspect release and failed to parse current version '%s' semver: %w", bi.Release, err)
-	}
-
-	if version.Prerelease() != "" {
-		if strings.HasPrefix(version.Prerelease(), "dev.") {
-			// special case for dev version; bump the patch down to determine the last release
-			if version.Patch() == 0 {
-				// patch should never be 0 on a dev version
-				return "", fmt.Errorf("Could not determine latest aspect release and failed to parse current version '%s' semver: %w", bi.Release, err)
-			}
-			return fmt.Sprintf("%v.%v.%v", version.Major(), version.Minor(), version.Patch()-1), nil
-		}
-	}
-
-	return version.String(), nil
-}
-
-func (runner *Init) Run(ctx context.Context, _ *cobra.Command, args []string) error {
+func (runner *Init) Run(ctx context.Context, cmd *cobra.Command, args []string) error {
 	wd, err := os.Getwd()
 	if err != nil {
 		return err
@@ -108,117 +60,23 @@ func (runner *Init) Run(ctx context.Context, _ *cobra.Command, args []string) er
 		return nil
 	}
 
-	// figure out what version of Aspect to stamp out
-	aspectVersion, err := runner.lookupAspectVersion()
+	preset, _ := cmd.Flags().GetString("preset")
+	isInteractiveMode, err := cmd.Root().PersistentFlags().GetBool(flags.AspectInteractiveFlagName)
 	if err != nil {
 		return err
 	}
 
-	if len(args) > 0 {
-		return initNewWorkspace(args[0], aspectVersion)
-	}
+	ctrl := &commands.Controller{}
+	// TODO: wire any relevant options from Aspect config.yaml
+	rc := &scaffold.ScaffoldRC{}
+	ctrl.Prepare(engine.New(), rc)
+	// TODO: set log level to match our context
+	log.Logger = log.Level(zerolog.WarnLevel)
+	// TODO: make the remote template configurable?
+	args = append(args, "https://github.com/aspect-build/aspect-workflows-template.git")
 
-	prompt := promptui.Select{
-		Label: "Where would you like to create a Bazel workspace",
-		Items: []string{
-			fmt.Sprintf("Current directory (%s)", wd),
-			"Create a new directory under " + wd,
-		},
-	}
-
-	choice, _, err := prompt.Run()
-
-	if err != nil {
-		return fmt.Errorf("user aborted the wizard: %w", err)
-	}
-
-	if choice == 0 {
-		return initNewWorkspace(".", aspectVersion)
-	}
-	if choice == 1 {
-		prompt := promptui.Prompt{
-			Label: "Name for the new folder",
-		}
-
-		folder, err := prompt.Run()
-
-		if err != nil {
-			return fmt.Errorf("user aborted the wizard: %w", err)
-		}
-		return initNewWorkspace(folder, aspectVersion)
-	}
-	return fmt.Errorf("illegal state: no choices matched, please file a bug")
-}
-
-func initNewWorkspace(folder string, aspectVersion string) error {
-	var cdmsg string
-	if folder != "." {
-		fmt.Printf("Creating folder %s...\n", folder)
-		if err := os.Mkdir(folder, 0755); err != nil {
-			return fmt.Errorf("failed to create directory %s: %w", folder, err)
-		}
-		os.Chdir(folder)
-		cdmsg = fmt.Sprintf("run 'cd %s', then ", folder)
-	}
-	if err := os.MkdirAll(path.Join(".aspect", "bazelrc"), 0755); err != nil {
-		return fmt.Errorf("failed to create directory: %w", err)
-	}
-
-	err := fs.WalkDir(Content, ".", func(tmpl string, f fs.DirEntry, err error) error {
-		if err != nil {
-			return fmt.Errorf("unable to list embedded files: %w", err)
-		}
-		out := strings.TrimPrefix(tmpl, "template/")
-		if f.IsDir() {
-			os.Mkdir(out, 0755)
-		} else {
-			content, err := Content.ReadFile(tmpl)
-			if err != nil {
-				return fmt.Errorf("unable to read template file %s: %w", tmpl, err)
-			}
-			if f.Name() == ".bazeliskrc" {
-				// replace the {{aspect_version}} token in the bazeliskrc template to the desired aspect version
-				// TODO: use https://pkg.go.dev/text/template instead
-				content = []byte(strings.Replace(string(content), "{{aspect_version}}", aspectVersion, 1))
-			}
-			var mode fs.FileMode
-			if strings.HasSuffix(out, ".sh") {
-				mode = 0755
-			} else {
-				mode = 0644
-			}
-			if err = os.WriteFile(out, content, mode); err != nil {
-				return fmt.Errorf("failed to write file: %w", err)
-			} else {
-				fmt.Printf("wrote %s\n", out)
-			}
-		}
-		return nil
+	return ctrl.New(args, commands.FlagsNew{
+		NoPrompt: !isInteractiveMode,
+		Preset:   preset,
 	})
-	if err != nil {
-		return fmt.Errorf("unable to walk embed folder: %w", err)
-	}
-
-	rcs, err := bazelrc.Content.ReadDir(".")
-	if err != nil {
-		return fmt.Errorf("unable to list embed files: %w", err)
-	}
-
-	for _, p := range rcs {
-		f := path.Join(".aspect", "bazelrc", p.Name())
-		content, err := bazelrc.Content.ReadFile(p.Name())
-		if err != nil {
-			return fmt.Errorf("failed to read embedded data: %w", err)
-		}
-		if err = os.WriteFile(f, content, 0644); err != nil {
-			return fmt.Errorf("failed to write file: %w", err)
-		} else {
-			fmt.Printf("wrote %s\n", f)
-		}
-	}
-
-	boldCyan.Println("Finished creating Bazel workspace.")
-	fmt.Printf("To confirm this is working, %srun 'bazel fetch //...'\n", cdmsg)
-	fmt.Println("Next step: create BUILD.bazel files. Consider 'aspect configure' to automate this.")
-	return nil
 }
