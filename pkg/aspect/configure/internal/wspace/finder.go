32fb0945e01c6d179017d740aaef1210cf873e4b
Jason Bedard <jason@aspect.dev>
2024-06-06 08:59:01 -0700

    refactor: align sync cli configure code with gazelle (#5649)
    The cli configure code copied from gazelle hasn't been properly kept up
    to date the last few gazelle upgrades.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    
    GitOrigin-RevId: 6676d12da96a1d1010d7d1f92ebcb11fb1db7d22

--- pkg/aspect/configure/internal/wspace/finder.go
+++ pkg/aspect/configure/internal/wspace/finder.go
@@ -1,4 +1,16 @@
-// Copy of gazelle internal https://github.com/bazelbuild/bazel-gazelle/blob/b62589672b5c32264ddf40585247d684c29bdd15/internal/wspace/finder.go
+/* Copyright 2016 The Bazel Authors. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+   http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// NOTE: synced from bazel-gazelle/internal/wspace/finder.go
 
 // Package wspace provides functions to locate and modify a bazel WORKSPACE file.
 package wspace
