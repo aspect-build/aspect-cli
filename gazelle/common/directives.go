3561b073738c0af88d9c7f983826c0972ec58195
Jason Bedard <jason@aspect.dev>
2024-06-24 04:33:34 -0700

    refactor(gazelle): move common enable/disable directive logic to common (#5820)
    GitOrigin-RevId: dc27b4634a7653ba8c16cfedd7266ccc645d7b3c

--- gazelle/common/directives.go
+++ gazelle/common/directives.go
@@ -1,5 +1,12 @@
 package gazelle
 
+import (
+	"log"
+	"strings"
+
+	"github.com/bazelbuild/bazel-gazelle/rule"
+)
+
 const (
 	// Directive_GenerationMode represents the directive that controls the BUILD generation
 	// mode. See below for the GenerationModeType constants.
@@ -20,3 +27,15 @@ const (
 	// Create: create new and updating existing BUILD files
 	GenerationModeCreate GenerationModeType = "create"
 )
+
+func ReadEnabled(d rule.Directive) bool {
+	switch strings.TrimSpace(d.Value) {
+	case "enabled":
+		return true
+	case "disabled":
+		return false
+	default:
+		log.Fatalf("Invalid directive %s enabled/disabled value: %s", d.Key, d.Value)
+		return false
+	}
+}
