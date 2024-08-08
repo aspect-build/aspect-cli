cf4b7f63a0f9ab8ca33fd2db8f86d94f047a6ad2
Jason Bedard <jason@aspect.dev>
2024-08-08 08:33:01 -0700

    chore(cli): upgrade bazel-gazelle to 0.38 (#6316)
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    
    GitOrigin-RevId: 35e79e61924bf0b71905c53210ab8cbe32e059ff

--- gazelle/common/excludes.go
+++ gazelle/common/excludes.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Required for using go:linkname below for using the private isExcluded.
-// walkConfig: https://github.com/bazelbuild/bazel-gazelle/blob/v0.37.0/walk/config.go#L41-L46
+// walkConfig: https://github.com/bazelbuild/bazel-gazelle/blob/v0.38.0/walk/config.go#L41-L46
 type walkConfig struct {
 	excludes []string
 	// Below are fields that are not used by the isExcluded function but match the walkConfig
@@ -19,7 +19,7 @@ type walkConfig struct {
 	_ sync.Once // loadOnce sync.Once
 }
 
-// isExcluded: https://github.com/bazelbuild/bazel-gazelle/blob/v0.37.0/walk/config.go#L54-L59
+// isExcluded: https://github.com/bazelbuild/bazel-gazelle/blob/v0.38.0/walk/config.go#L54-L59
 //
 //go:linkname isExcluded github.com/bazelbuild/bazel-gazelle/walk.(*walkConfig).isExcluded
 func isExcluded(wc *walkConfig, rel, base string) bool
