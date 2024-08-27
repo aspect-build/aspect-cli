9f7f43286577aaa4fb9fe54e941f5937b278e1d0
Jason Bedard <jason@aspect.dev>
2024-08-27 13:41:29 -0700

    fix(cli): passthru configure cli arguments to gazelle (#6566)
    Passthru all aspect-cli args to gazelle to support updating only
    specific directories.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    - Manual testing; please provide instructions so we can reproduce: run
    `//cli/pro cli` and ensure it only updates that directory and has a
    noticeable performance improvement
    
    GitOrigin-RevId: f1e79d2a2ac888488f463d29e014617221fd70b5

--- pkg/aspect/configure/configure.go
+++ pkg/aspect/configure/configure.go
@@ -140,6 +140,9 @@ configure:
 		fixArgs = append(fixArgs, "--memprofile="+memprofile)
 	}
 
+	// Append additional args including specific directories to fix.
+	fixArgs = append(fixArgs, args...)
+
 	if mode == "fix" {
 		fmt.Fprintf(runner.Streams.Stdout, "Updating BUILD files for %s\n", strings.Join(runner.languageKeys, ", "))
 	}
