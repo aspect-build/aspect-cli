af217bde05f27b0aae408d211c18603c8e449046
Greg Magolan <greg@aspect.dev>
2024-09-10 12:38:21 -0700

    chore: remove homebrew from release artifacts and switch to monorepo versioning

--- .github/workflows/install_snippet.sh
+++ .github/workflows/install_snippet.sh
@@ -7,26 +7,22 @@ set -o errexit -o nounset -o pipefail
 TAG=${GITHUB_REF_NAME}
 
 cat <<EOF
-## Install [Aspect CLI](https://www.aspect.build/cli)
+## Install Aspect CLI OSS
 
 See full install instructions in [README.md](https://github.com/aspect-build/aspect-cli/blob/${TAG}/README.md).
 
-### Homebrew (MacOS)
-
-Link the [Aspect CLI](https://www.aspect.build/cli) as \`bazel\` just like the [bazelisk](https://github.com/bazelbuild/bazelisk) installer does:
-
-\`\`\`
-% brew install aspect-build/aspect/aspect
-\`\`\`
-
 ### Bazelisk (MacOS / Linux)
 
-Configure [bazelisk](https://github.com/bazelbuild/bazelisk) to use the [Aspect CLI](https://www.aspect.build/cli) for all developers. Add this to \`.bazeliskrc\` in your project folder:
+Configure [bazelisk](https://github.com/bazelbuild/bazelisk) to use the Aspect CLI OSS for all developers in a repository
+by adding the following to \`.bazeliskrc\` in the repository root:
 
 \`\`\`
 BAZELISK_BASE_URL=https://github.com/aspect-build/aspect-cli/releases/download
 USE_BAZEL_VERSION=aspect/${TAG}
 \`\`\`
+
+> [!NOTE]
+> This approach doesn't provide the \`aspect init\` command, which has to run outside a Bazel workspace.
 
 The underlying version of Bazel can be configured in your \`.bazelversion\` file or the \`BAZEL_VERSION\` environment variable.
 
