ac8855ae4a0eb85f320a417349dac0929460a503
Jason Bedard <jason@aspect.dev>
2024-10-09 14:28:04 -0700

    test(cli): gazelle triple slash test cases (#7057)
    Various test cases trying to reproduce a customer issue.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - New test cases added
    
    GitOrigin-RevId: 0e04447116b665e83d89a0e91b9931631f5e44f4

--- gazelle/js/tests/npm_types_package/triple_slash/ref.ts
+++ gazelle/js/tests/npm_types_package/triple_slash/ref.ts
@@ -1,4 +1,16 @@
+/// References to npm packages, via both "lib" and "types"
 /// <reference lib="jquery">
 /// <reference types="@testing-library/jest-dom">
-/// <reference path="../only_types/only">
 
+// Transpiled .ts files, referenced both with and without the .d.ts extension.
+// IDEs flag this as an error that should be a regular `import` but this is still valid.
+/// <reference path="../transpiled/types">
+/// <reference path="../transpiled/types.d.ts">
+
+// ... same with .d.ts source files.
+/// <reference path="../only_types/types">
+/// <reference path="../only_types/types.d.ts">
+
+// ... same with source files with the same BUILD.
+/// <reference path="./defs">
+/// <reference path="./defs.d.ts">
