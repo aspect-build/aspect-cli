c74279f3b73c8c77f63f975fe84d14388526c280
Jason Bedard <jason+github@jbedard.ca>
2024-10-29 10:16:29 -0700

    refactor(cli): upgrade configure directive suggestions to use aspect directives

--- gazelle/js/generate.go
+++ gazelle/js/generate.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gazelle
 
 import (
@@ -384,10 +400,10 @@ func (ts *typeScriptLang) addProjectRule(cfg *JsGazelleConfig, tsconfigRel strin
 	colError := gazelle.CheckCollisionErrors(targetName, TsProjectKind, sourceRuleKinds, args)
 	if colError != nil {
 		return nil, fmt.Errorf(colError.Error()+" "+
-			"Use the '# gazelle:%s' directive to change the naming convention.\n\n"+
+			"Use the '# aspect:%s' directive to change the naming convention.\n\n"+
 			"For example:\n"+
-			"\t# gazelle:%s {dirname}_js\n"+
-			"\t# gazelle:%s {dirname}_js_tests",
+			"\t# aspect:%s {dirname}_js\n"+
+			"\t# aspect:%s {dirname}_js_tests",
 			Directive_LibraryNamingConvention,
 			Directive_LibraryNamingConvention,
 			Directive_TestsNamingConvention,
