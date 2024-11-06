28c4d80a7ff4b641af6a817adadfa4bd309ba509
Jason Bedard <jason@aspect.dev>
2024-11-05 17:22:11 -0800

    refactor(cli): add basic configure tsconfig logging (#7237)
    This would have been useful recently when debugging tsconfig issues.
    
    ---
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    
    GitOrigin-RevId: fae3a0d76857bdaf185fc30d7953c7da538521ba

--- gazelle/js/typescript/config.go
+++ gazelle/js/typescript/config.go
@@ -7,6 +7,7 @@ import (
 
 	node "aspect.build/cli/gazelle/js/node"
 	pnpm "aspect.build/cli/gazelle/js/pnpm"
+	BazelLog "aspect.build/cli/pkg/logger"
 )
 
 type workspacePath struct {
@@ -46,6 +47,8 @@ func (tc *TsWorkspace) AddTsConfigFile(root, rel, fileName string) {
 		return
 	}
 
+	BazelLog.Debugf("Adding tsconfig file %s/%s", rel, fileName)
+
 	tc.cm.configFiles[rel] = &workspacePath{
 		root:     root,
 		rel:      rel,
@@ -77,6 +80,8 @@ func (tc *TsWorkspace) GetTsConfigFile(rel string) *TsConfig {
 		fmt.Printf("Failed to parse tsconfig file %s: %v\n", path.Join(p.rel, p.fileName), err)
 		return nil
 	}
+
+	BazelLog.Debugf("Parsed tsconfig file %s/%s", p.rel, p.fileName)
 
 	return c
 }
