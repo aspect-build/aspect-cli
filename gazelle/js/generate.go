17394c643196ac7c7bb6fbb7fa56c85595004b4f
Jason Bedard <jason@aspect.dev>
2024-12-03 10:32:57 -0800

    feat(cli): custom `tsconfig.json` file name (#7484)
    Synced from https://github.com/aspect-build/aspect-cli/pull/784 by
    henkerik:
    
    ---
    
    ### Changes are visible to end-users: yes
    
    - Searched for relevant documentation and updated as needed: no
    - Breaking change (forces users to change their own code or config): no
    - Suggested release notes appear below: yes
    
    ```
    # gazelle:js_tsconfig_file _tsconfig_json_
    
    Path to the `tsconfig.json` file, in case your TSConfig file has a different file name
    ```
    
    ### Test plan
    
    - New test cases added: `bazel test
    //gazelle/js:tsconfig_custom_file_name_test`
    
    Closes aspect-build/aspect-cli#784
    
    Co-authored-by: Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
    GitOrigin-RevId: b773c2626f32ffa826f85ed52e31472acad93a7c

--- gazelle/js/generate.go
+++ gazelle/js/generate.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Aspect Build Systems, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package gazelle
 
 import (
@@ -314,7 +330,7 @@ func (ts *typeScriptLang) collectTsConfigImports(cfg *JsGazelleConfig, args lang
 
 	for _, reference := range tsconfig.References {
 		// TODO: how do we know the referenced tsconfig filename?
-		referenceFile := cfg.defaultTsconfigName
+		referenceFile := cfg.tsconfigName
 
 		imports = append(imports, ImportStatement{
 			ImportSpec: resolve.ImportSpec{
