9679cb625ff12f3d19c02b07bbcbe3efe6eff94b
Jason Bedard <jason@aspect.build>
2025-03-17 22:21:26 -0700

    feat(cli): support orion plugins importing other starlark files (#8203)
    Support loading other starlark files with paths relative to the
    workspace root, optionally prefixed with `//`.
    
    Today `load()` does actually work, but this adds official support and
    fixes the following issues:
    * paths are always relative to the cwd, they should be workspace
    relative
    * the `aspect.*` and other builtin globals are not available in
    `load()`ed files
    
    Close #8202
    
    ---
    
    ### Changes are visible to end-users: yes
    
    - Searched for relevant documentation and updated as needed: yes
    - Breaking change (forces users to change their own code or config): no
    - Suggested release notes appear below: yes
    
    Support `load("//workspace/relative/path/utils.star", "thing")` style
    load statements in Orion starlark extensions.
    
    ### Test plan
    
    - Covered by existing test cases
    - Manual testing; please provide instructions so we can reproduce: run
    `configure` in client repository, run in subdirectories
    
    GitOrigin-RevId: fc8fac16f41cdb77f2468ced8c5e82152f8c4873

--- gazelle/common/starlark/eval_test.go
+++ gazelle/common/starlark/eval_test.go
@@ -9,9 +9,10 @@ import (
 )
 
 func run(t *testing.T, code string) (starlark.StringDict, error) {
-	testFile := path.Join(os.TempDir(), "test.star")
+	testDir := os.TempDir()
+	testFile := "test.star"
 
-	f, err := os.Create(testFile)
+	f, err := os.Create(path.Join(testDir, testFile))
 	if err != nil {
 		t.Errorf("Temp star create failure: %v", err)
 	}
@@ -26,7 +27,7 @@ func run(t *testing.T, code string) (starlark.StringDict, error) {
 		t.Errorf("Temp star close failure: %v", err)
 	}
 
-	return Eval(testFile, make(map[string]starlark.Value), make(map[string]interface{}))
+	return Eval(testDir, testFile, make(map[string]starlark.Value), make(map[string]interface{}))
 }
 
 func runOk(t *testing.T, code string) starlark.StringDict {
@@ -76,35 +77,6 @@ y = "s"
 		ystr, _ := starlark.AsString(y)
 		if ystr != "s" {
 			t.Errorf("Expected string 's', got %v", ystr)
-		}
-	})
-
-	t.Run("basic func", func(t *testing.T) {
-		res := runOk(t, `
-def f():
-	return 1
-`)
-		f, hasF := res["f"]
-		if !hasF {
-			t.Errorf("Expected f to be defined")
-		}
-
-		if f.Type() != "function" {
-			t.Errorf("Expected type function %v", f.Type())
-		}
-
-		v, err := Call(f, starlark.Tuple{}, nil)
-		if err != nil {
-			t.Errorf("Expected call to succeed %v", err)
-		}
-
-		i, intErr := starlark.AsInt32(v)
-		if intErr != nil {
-			t.Errorf("Expected int %v", intErr)
-		}
-
-		if i != 1 {
-			t.Errorf("Expected 1, got %v", i)
 		}
 	})
 }
