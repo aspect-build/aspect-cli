d11c942c9a8fd27496262ac980b51062599cc069
Jason Bedard <jason@aspect.build>
2025-07-18 16:34:18 -0700

    perf(cli): cache BUILD-generation parsing of package.json (#9125)
    Another file read+parse+process that can be cached. For some repos with
    1500+ projects in the pnpm workspace this should be noticeable.
    
    Also adding some tests.
    
    ### Changes are visible to end-users: yes
    
    - Searched for relevant documentation and updated as needed: yes
    - Breaking change (forces users to change their own code or config): no
    - Suggested release notes appear below: yes
    
    Cache parsing of of package.json during BUILD generation.
    
    ### Test plan
    
    - Covered by existing test cases
    - New test cases added
    
    GitOrigin-RevId: 4314ffbd8eeef24a05d46275b2a14ab77fd0e3af

---
+++ gazelle/js/node/package_test.go
@@ -0,0 +1,45 @@
+package gazelle
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestParsePackageJsonImports(t *testing.T) {
+	t.Run("basic file refs", func(t *testing.T) {
+		assertParsePackageJsonImports(t, `{"main":"foo.js"}`, "foo.js")
+		assertParsePackageJsonImports(t, `{"main":"foo/bar.js"}`, "foo/bar.js")
+		assertParsePackageJsonImports(t, `{"main":"foo/../bar.js"}`, "bar.js")
+		assertParsePackageJsonImports(t, `{"types":"foo.d.ts"}`, "foo.d.ts")
+		assertParsePackageJsonImports(t, `{"typings":"foo.d.ts"}`, "foo.d.ts")
+	})
+
+	t.Run("package exports", func(t *testing.T) {
+		assertParsePackageJsonImports(t, `{"exports":{"entry-name":"./foo.js"}}`, "foo.js")
+		assertParsePackageJsonImports(t, `{"exports":{"./subpath":"./foo.js"}}`, "foo.js")
+		assertParsePackageJsonImports(t, `{"exports":{"./subpath":null}}`)
+		assertParsePackageJsonImports(t, `{"exports":{"./subpath":"./foo.js","./subpath2":"./bar.js"}}`, "foo.js", "bar.js")
+	})
+
+	t.Run("invalid exports", func(t *testing.T) {
+		assertParsePackageJsonImports(t, `{"exports":{"./subpath":123, "x": []}}`)
+	})
+}
+
+func assertParsePackageJsonImports(t *testing.T, packageJson string, expectedImports ...string) {
+	imps, err := ParsePackageJsonImports(strings.NewReader(packageJson))
+
+	if err != nil {
+		t.Errorf("ParsePackageJsonImports failed: %v:\n\t%s", err, packageJson)
+		return
+	}
+	if len(imps) != len(expectedImports) {
+		t.Errorf("ParsePackageJsonImports expected %d imports, got %d:\n\t%s", len(expectedImports), len(imps), packageJson)
+		return
+	}
+	for i, expected := range expectedImports {
+		if imps[i] != expected {
+			t.Errorf("ParsePackageJsonImports(%q) expected import %d to be %q, got %q", packageJson, i, expected, imps[i])
+		}
+	}
+}
