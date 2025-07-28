0cfd79a81530123dca0b4790c59d7288884e3541
Jason Bedard <jason@aspect.build>
2025-07-28 10:36:16 -0700

    fix(cli): do not traverse into subdirs containing BUILD when evaluating globs (#9155)
    The last change to [use the new `walk.GetDirInfo`
    API](https://github.com/aspect-build/silo/commit/7f9d26e93692cb70fbe4702749fb6c5589f85f20)
    did not take into account that globs need to stop descending when
    reaching directories containing a BUILD.
    
    ### Changes are visible to end-users: no
    
    ### Test plan
    
    - Covered by existing test cases
    - New test cases added
    
    GitOrigin-RevId: 2e106a4742d8274ad2f141399fa0b9ef2fda3778

--- gazelle/js/tests/simple_json_import/main.ts
+++ gazelle/js/tests/simple_json_import/main.ts
@@ -1,5 +1,8 @@
 import * as data from './data.json';
 import * as data2 from './subdir/data.json';
 import * as data3 from './subproject/data.json';
+import * as data4 from './subdir-globs/a.json';
+import * as data5 from './subdir-globs/sub1/b.json';
+import * as data6 from './subdir-globs/sub2/c.json';
 
-console.log(data, data2, data3);
+console.log(data, data2, data3, data4, data5, data6);
