71487cdf914f766ef2a76000c5189897c9a475b5
Greg Magolan <greg@aspect.build>
2025-11-06 19:11:59 -0800

    Handle re-directs in cli usage telemetry

--- crates/aspect-cli/src/telemetry.rs
+++ crates/aspect-cli/src/telemetry.rs
@@ -1,7 +1,7 @@
 use aspect_config::{cargo_pkg_version, BZLARCH, BZLOS, TELURL};
 use reqwest::header::HeaderName;
 use reqwest::redirect::Policy;
-use reqwest::{self, Method};
+use reqwest::{self, Method, StatusCode};
 use std::env::var;
 use std::time::Duration;
 
@@ -17,20 +17,40 @@ pub async fn send_telemetry() -> std::result::Result<(), ()> {
         "{{\"cli\": {{\"version\": \"{v}\", \"os\": \"{BZLOS}\", \"arch\": \"{BZLARCH}\"}}}}"
     );
 
-    let url = TELURL.to_string();
+    let mut url = TELURL.to_string();
     let client = reqwest::Client::builder()
         .redirect(Policy::limited(10))
         .build()
         .unwrap();
 
-    let req = client
-        .request(Method::POST, &url)
-        .query(&[("source", "aspect-cli")])
-        .header(HeaderName::from_static("content-type"), "application/json")
-        .header(HeaderName::from_static("user-agent"), "reqwest;aspect-cli")
-        .body(body.clone())
-        .timeout(Duration::from_secs(5));
+    loop {
+        let req = client
+            .request(Method::POST, &url)
+            .query(&[("source", "aspect-cli")])
+            .header(HeaderName::from_static("content-type"), "application/json")
+            .header(HeaderName::from_static("user-agent"), "reqwest;aspect-cli")
+            .body(body.clone())
+            .timeout(Duration::from_secs(5));
 
-    req.send().await.map_err(|_| ())?;
+        let send_res = req.send().await;
+
+        let send_res = match send_res {
+            Ok(r) => r,
+            Err(_) => break,
+        };
+
+        match send_res.status() {
+            StatusCode::FOUND | StatusCode::PERMANENT_REDIRECT | StatusCode::TEMPORARY_REDIRECT => {
+                if let Some(loc) = send_res.headers().get("location") {
+                    if let Ok(loc_str) = loc.to_str() {
+                        url = loc_str.to_owned();
+                        continue;
+                    }
+                }
+                break;
+            },
+            _ => break
+        };
+    }
     Ok(())
 }
