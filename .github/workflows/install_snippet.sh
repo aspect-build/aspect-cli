c389e72518a36f7dc96e6bc38a00159ba752e8a5
Alex Eagle <alex@aspect.dev>
2025-12-11 08:35:21 -0800

    fix(release): provide explicit triggering

--- .github/workflows/install_snippet.sh
+++ .github/workflows/install_snippet.sh
@@ -2,10 +2,6 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
-
 cat <<EOF
 ### Install Aspect CLI (MacOS and Linux)
 
